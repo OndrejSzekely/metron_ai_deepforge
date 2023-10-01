f58610bf934fb0510f999bca9706023e31d469f6
ondra <ondra.szekely@gmail.com>
2023-10-01 14:56:40 +0200

    feat: Add logging func

---
+++ use_cases/asterisk/utils/logging_functions.py
@@ -0,0 +1,10 @@
+"""Miscelanneous logging functions.
+"""
+
+import logging
+
+def log_kfold_split_info(train_images_num: int, val_images_num: int) -> None:
+    logging.info("*"*15 + " SPLIT INFO " + "*"*15)
+    logging.info(f"Training images: {train_images_num}")
+    logging.info(f"Val images: {val_images_num}")
+    logging.info("*"*42)
