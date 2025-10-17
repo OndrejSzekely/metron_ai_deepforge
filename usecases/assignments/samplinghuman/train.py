bddd610046a6c1129f6395a8c6f3b4fd1e53c1be
ondra <ondra.szekely@gmail.com>
2025-10-17 09:25:25 +0000

    arch: Fix TF NumPy issue & mount data

--- usecases/assignments/samplinghuman/train.py
+++ usecases/assignments/samplinghuman/train.py
@@ -2,3 +2,7 @@
 # Copyright (c) 2025 Ondrej Szekely (ondra.szekely@gmail.com).
 
 """SampleHuman Assignment"""
+
+from usecases.assignments.samplinghuman.utils.data import Imagenet64
+
+og_dataset = Imagenet64("/mnt/samplinghuman_data")
